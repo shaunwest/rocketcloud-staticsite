b938615133f832f7a28a517b3f0711971309274b
Shaun West <shaunwest@quickenloans.com>
2022-03-30 16:48:34 -0400

    Updated

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 console.log('hello world!');
 
+const foodList = document.querySelector('#food-list');
+
 fetch('http://localhost:3000/main/food', { mode: 'cors'})
-  .then((response) => response.text())
-  .then((text) => console.log(text));
+  .then((response) => response.json())
+  .then((food) => food.forEach((item) => {
+    const li = document.createElement('li');
+    li.innerHTML = item;
+    foodList.appendChild(li);
+  }));
