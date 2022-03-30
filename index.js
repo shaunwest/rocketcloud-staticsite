557c2407143d530fa3db631955c0735ad198be0e
Shaun West <shaunwest@quickenloans.com>
2022-03-30 16:33:31 -0400

    Updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log('hello world!');
 
-fetch('http://localhost:3001/main/food', { mode: 'cors'})
+fetch('http://localhost:3000/main/food', { mode: 'cors'})
   .then((response) => response.text())
   .then((text) => console.log(text));
